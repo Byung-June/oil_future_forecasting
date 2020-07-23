a5d939b6fa1de2c4f4c0d80fe107a342447913b3
kty <lkjs112@postech.ac.kr>
2020-07-23 17:25:46 +0900

    _utility.py updated
    * [function] rolling_sample
    input: time added
        if type(time) == int: int in range(window_num + sample_num - 2, len(data_) - 1)
        if type(time) == str: %Y-%m-%d format -> converted to int in algorithm
        if type(time) == Datetime -> converted to int in algorithm

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oil-future-forecasting',
+    version='0.0.1',
+    description='oil future forecasting',
+    author='Taeyoon Kim, Byungjune Kim',
+    author_email='lkjs112@postech.ac.kr, kbj219@postech.ac.kr',
+    url='https://github.com/Byung-June/oil_future_forecasting',
+    packages=find_packages()
+)
