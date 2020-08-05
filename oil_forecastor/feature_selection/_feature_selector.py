a9585647587b8b8c305059bffe0962f6001d1768
kty <lkjs112@postech.ac.kr>
2020-08-05 10:41:33 +0900

    UPDATE: Feature selection, SVR, KernelRidge
    * Added feature selector.
    * Added SVR and KernelRidge

--- oil_forecastor/feature_selection/_feature_selector.py
+++ oil_forecastor/feature_selection/_feature_selector.py
@@ -1,11 +1,24 @@
 # https://scikit-learn.org/stable/modules/feature_selection.html#
-from sklearn.feature_selection import SeleckKBest
-from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
 
 
-def select():
-    pass
+def selector(
+    X_train, X_test, y_train, y_test, n_features,
+    method='f-classif'
+):
+    if method == 'f-classif':
+        return _f_classif_square_selector(
+            X_train, X_test, y_train, y_test, n_features
+        )
+    else:
+        raise Exception("Not supported feature selectio method"
+                        "Do not use underscore _")
 
 
-def _chi_square_selector(X_train, y_train, k=20):
-    return SeleckKBest(chi2, k).fit_transform(X_train, y_train)
+def _f_classif_square_selector(X_train, X_test, y_train, y_test, n_features):
+    kbest = SelectKBest(f_classif, n_features)
+    kbest.fit(X_train, y_train)
+    X_train = X_train[:, kbest.get_support()]
+    X_test = X_test[:, kbest.get_support()]
+    return X_train, X_test, y_train, y_test
