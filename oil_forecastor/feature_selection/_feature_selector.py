dc04ec68b10f4c53f486cc8524fbc4965ed3c418
kty <lkjs112@postech.ac.kr, taeyoonkim94@gmail.com>
2020-08-25 10:01:15 +0900

    UPDATE: undo last commit
    * train test split returns list of dataframes

--- oil_forecastor/feature_selection/_feature_selector.py
+++ oil_forecastor/feature_selection/_feature_selector.py
@@ -7,22 +7,6 @@ def selector(
     X_train, X_test, y_train, y_test, n_features,
     method='f-classif'
 ):
-    """[summary]
-
-    Args:
-        X_train (array): n_X_train_samples x None
-        X_test (array): n_test_samples x None
-        y_train (array): n_y_train_samples x None
-        y_test (array): n_y_test_samples x None
-        n_features (int): number of features
-        method (str, optional): [description]. Defaults to 'f-classif'.
-
-    Raises:
-        Exception: [description]
-
-    Returns:
-        tuple: X_train_reduce, X_test_reduced, y_train_reduced, y_test_reduced
-    """
     if method == 'f-classif':
         return _f_classif_square_selector(
             X_train, X_test, y_train, y_test, n_features
