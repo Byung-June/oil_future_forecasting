19769d8bb0c89c2bd072c10dd13cef71135ba182
Byung-June <kbj219@postech.ac.kr>
2020-09-23 18:31:46 +0900

    update r2_oos.py
    upgrade visualization

--- examples/r2_oos.py
+++ examples/r2_oos.py
@@ -3,8 +3,9 @@ import numpy as np
 import glob
 from sklearn.metrics import r2_score
 from scipy.stats import t
-from statsmodels.tsa.stattools import acovf
+from statsmodels.tsa.stattools import adfuller
 import os
+
 
 
 def db_test(true, pred1, pred2, h, err_type='MSE'):
@@ -62,7 +63,7 @@ def pt_test(true, pred):
     return pt_stat, p_value
 
 
-def r2_oos_func(data_, type='tsa'):
+def r2_oos_func(data_, type='tsa', test_type='ladder'):
     if type=='tsa':
         data_ = pd.read_csv(data_, index_col=1)
         data_ = data_.drop(columns=['Unnamed: 0'])
@@ -74,22 +75,56 @@ def r2_oos_func(data_, type='tsa'):
 
     y_pred = data_.iloc[:, 0]
     y_test = data_.iloc[:, 1]
+    unit_root_test = [adfuller(y_pred - y_test)[1]]
+
     r2_oos = [r2_score(y_test, y_pred)]
     y_t = y_test.shift(1)
     pt_y = y_test - y_t
     pt_x = y_pred - y_t
     pt_p_value = [pt_test(pt_y, pt_x)[1]]
+    time = []
+    step = 3
+    T = len(y_test) // step
 
-    for n_dates in [120, 60, 36, 24, 12]:
-        y_pred = data_.iloc[-n_dates:, 0]
-        y_test = data_.iloc[-n_dates:, 1]
-        r2_oos.append(r2_score(y_test, y_pred))
+    if test_type=='step':
 
-        y_test = pt_y.iloc[-n_dates:]
-        y_pred = pt_x.iloc[-n_dates:]
-        pt_p_value.append(pt_test(y_test, y_pred)[1])
+        for n_dates in np.arange(1, step + 1)[::-1]:
+            start = - T * n_dates
+            end = - T * (n_dates - 1)
+            if end != 0:
+                y_pred = data_.iloc[start:end, 0]
+                y_test = data_.iloc[start:end, 1]
+                r2_oos.append(r2_score(y_test, y_pred))
+                unit_root_test.append(adfuller(y_pred - y_test)[1])
 
-    return r2_oos, pt_p_value
+                y_test = pt_y.iloc[start:end]
+                y_pred = pt_x.iloc[start:end]
+                pt_p_value.append(pt_test(y_test, y_pred)[1])
+                time.append(y_test.index[0])
+            else:
+                y_pred = data_.iloc[start:, 0]
+                y_test = data_.iloc[start:, 1]
+                r2_oos.append(r2_score(y_test, y_pred))
+                unit_root_test.append(adfuller(y_pred - y_test)[1])
+
+                y_test = pt_y.iloc[start:]
+                y_pred = pt_x.iloc[start:]
+                pt_p_value.append(pt_test(y_test, y_pred)[1])
+                time.append(y_test.index[0])
+
+    if test_type=='ladder':
+        for n_dates in [120, 60, 36, 24, 12]:
+            y_pred = data_.iloc[-n_dates:, 0]
+            y_test = data_.iloc[-n_dates:, 1]
+            r2_oos.append(r2_score(y_test, y_pred))
+            unit_root_test.append(adfuller(y_pred - y_test)[1])
+
+            y_test = pt_y.iloc[-n_dates:]
+            y_pred = pt_x.iloc[-n_dates:]
+            pt_p_value.append(pt_test(y_test, y_pred)[1])
+            time.append(y_test.index[0])
+
+    return time, np.round(r2_oos, 3), np.round(pt_p_value, 3), unit_root_test
 
 
 def r2_oos_ml(path='../results'):
@@ -144,19 +179,49 @@ def evaluation(df, y_true, delete_outlier=False):
 
 if __name__ == "__main__":
     path_tsa = os.getcwd()
-    path_ml = 'D:\Dropbox/6_git_repository\oil_future_forecasting/results/vol_ml_data_M_no_epu'
     file = '\*.csv'
+    test_type = 'ladder'
+    # test_type = 'step'
 
+    print('--------------------------------------')
+    print('ARIMAX')
+    print('--------------------------------------')
     data_list_tsa = glob.glob(path_tsa + file)
     for data in data_list_tsa:
+        print('--------------------------------------')
         print(data)
-        print(r2_oos_func(data))
+        result = r2_oos_func(data, test_type=test_type)
+        print(result[0])
+        print(result[1])
+        # print(result[2])
+        print(result[3])
 
+    print('--------------------------------------')
+    print('Machine Learning with EPU')
+    print('--------------------------------------')
+
+    path_ml = 'D:\Dropbox/6_git_repository\oil_future_forecasting/results/vol_ml_data_M'
     data_list_ml = glob.glob(path_ml + file)
     for data in data_list_ml:
         print('--------------------------------------')
         print(data)
-        result = r2_oos_func(data, type='ml')
+        result = r2_oos_func(data, type='ml', test_type=test_type)
         print(result[0])
-        # print(result[1])
+        print(result[1])
+        # print(result[2])
+        print(result[3])
 
+    print('--------------------------------------')
+    print('Machine Learning without EPU')
+    print('--------------------------------------')
+
+    path_ml = 'D:\Dropbox/6_git_repository\oil_future_forecasting/results/vol_ml_data_M_no_epu'
+    data_list_ml = glob.glob(path_ml + file)
+    for data in data_list_ml:
+        print('--------------------------------------')
+        print(data)
+        result = r2_oos_func(data, type='ml', test_type=test_type)
+        print(result[0])
+        print(result[1])
+        # print(result[2])
+        print(result[3])
--- examples/tsa_main.py
+++ examples/tsa_main.py
@@ -85,8 +85,8 @@ class GenModel:
 if __name__ == '__main__':
     ######
     # data = pd.read_csv('../data_preprocessing/bi_vol_ml_data_M_w3_d10_i10.csv', index_col=0)
-    # data = pd.read_csv('../data_preprocessing/vol_ml_data_M.csv', index_col=0)
-    data = pd.read_csv('../data_preprocessing/vol_ml_data_M_no_epu.csv', index_col=0)
+    data = pd.read_csv('../data_preprocessing/vol_ml_data_M.csv', index_col=0)
+    # data = pd.read_csv('../data_preprocessing/vol_ml_data_M_no_epu.csv', index_col=0)
     data = data.drop(['crude_future_daily_lag0',
                       # 'crude_future_daily_lag1', 'crude_future_daily_lag2',
                       # 'crude_future_daily_lag3', 'crude_future_daily_lag4'
@@ -102,13 +102,14 @@ if __name__ == '__main__':
     warnings.filterwarnings("ignore", category=RuntimeWarning)
 
     wsf_list = [
-        [3, 12, 10],
-        [3, 18, 10],
-        [6, 18, 10],
-        [3, 24, 10],
-        [6, 24, 10],
-        [9, 24, 10],
-        [12, 24, 10]
+        # [3, 12, 10],
+        # [3, 18, 10],
+        # [6, 18, 10],
+        # [3, 24, 10],
+        # [6, 24, 10],
+        # [9, 24, 10],
+        # [12, 24, 10]
+        [5, 5, 0]
     ]
 
     for w, s, f in wsf_list:
@@ -118,5 +119,6 @@ if __name__ == '__main__':
         g = GenModel(data, window_num=input_w, sample_num=input_s, forecast_period=1, feature_num=input_f,
                      denoise=None)
         arma = pd.DataFrame(g.gen_model('arima', process_num=4))
-        arma.to_csv('vol_arima_M_%s_%s_%s_no_epu.csv' % (input_w, input_s, input_f))
+        # arma.to_csv('vol_arima_M_%s_%s_%s_no_epu.csv' % (input_w, input_s, input_f))
+        arma.to_csv('vol_arima_M_%s_%s_%s.csv' % (input_w, input_s, input_f))
         print('result', arma)
